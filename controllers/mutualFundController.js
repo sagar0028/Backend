8f2bf6138e0c0defe39adb845dd0f3a753d7c1ec
Sagar Sharma <53833815+sagar0028@users.noreply.github.com>
2022-10-03 17:21:19 +0530

    Update mutualFundController.js

--- controllers/mutualFundController.js
+++ controllers/mutualFundController.js
@@ -6,7 +6,6 @@ const db = require("../connection/db");
 const loginDetails = async (req, res, next) => {
   try {
     let { UserName, UserType, password, Email, Mobile, Source } = req.body;
-    // console.log(UserName, UserType, password, Email, Mobile, Source);
     const _data = await db.formDetails.create({
       UserName: UserName,
       UserType: UserType,
@@ -84,7 +83,6 @@ const getDataBYID = async (req, res, next) => {
     const final = [];
     const obj = {};
     const result = await axios.get("https://api.mfapi.in/mf");
-    // console.log(result);
     const found = result.data.find((element) => element.schemeCode === id);
     if (found) {
       let result1 = await axios.get(`https://api.mfapi.in/mf/${id}`);
@@ -125,42 +123,3 @@ module.exports = {
   loginDetails,
   getProductData
 };
-
-// {
-//   "_id": {
-//     "$oid": "63342350d8de286f557ea7fb"
-//   },
-//   "amc_code": "400001",
-//   "amc": "Baroda BNP Paribas Asset Management India Pvt. Ltd.",
-//   "fund": "Baroda BNP Paribas Mutual Fund",
-//   "srno": "1",
-//   "office_type": "Registered Office",
-//   "add1": "Crescenzo, 7th Floor,",
-//   "add2": "G-Block, Bandra Kurla Complex,",
-//   "add3": "Mumbai - 400051",
-//   "email": "customer.care@bnpparibasmf.in",
-//   "phone": "91-22-3370 4000",
-//   "fax": "91-22-3370 4294",
-//   "webiste": "https://www.barodabnpparibasmf.in",
-//   "setup_date": "2004-04-15 00:00:00.000",
-//   "mf_type": "Pvt. Sector - Joint Ventures ( Foreign)",
-//   "trustee_name": "Baroda BNP Paribas Trustee India Pvt. Ltd.",
-//   "sponsor_name": "BNP Paribas Asset Management Asia Limited",
-//   "amc_inc_date": "2003-11-04 00:00:00.000",
-//   "s_name": "Baroda BNP Paribas",
-//   "amc_symbol": "BNPP MF",
-//   "city": "Mumbai",
-//   "rtamccode": "M",
-//   "flag": "A",
-//   "createdAt": {
-//     "$date": {
-//       "$numberLong": "1664361296380"
-//     }
-//   },
-//   "updatedAt": {
-//     "$date": {
-//       "$numberLong": "1664361296380"
-//     }
-//   },
-//   "__v": 0
-// }
